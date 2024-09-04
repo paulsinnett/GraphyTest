c40a4cd223bf3ac0402b8b23e51ab88b7f9eb668
Paul Sinnett <paul.sinnett@gmail.com>
2024-09-04 11:54:57 +0100

    simulate load
    this uses a random sleep time from a Poisson distribution to emulate variable load

--- Assets/CPULoad.cs
+++ Assets/CPULoad.cs
@@ -1,12 +1,44 @@
 using System.Threading;
 using UnityEngine;
 
+public class Poisson
+{
+    float lambda;
+
+    public Poisson(float lambda)
+    {
+        this.lambda = lambda;
+    }
+
+    public int GetRandomValue()
+    {
+        // Algorithm due to Donald Knuth, 1969.
+        float p = 1.0f;
+        float L = Mathf.Exp( -lambda );
+        int k = 0;
+        do
+        {
+            k++;
+            p *= Random.value;
+        }
+        while (p > L);
+        return k - 1;
+    }
+}
+
 public class CPULoad : MonoBehaviour
 {
-    public int perFrameLoad; // ms
+    public float minLoadPerFrame; // ms
+    public float meanLoadPerFrame; // ms
+    Poisson poisson;
+
+    void Start()
+    {
+        poisson = new Poisson( meanLoadPerFrame - minLoadPerFrame );
+    }
 
     void Update()
     {
-        Thread.Sleep(perFrameLoad);
+        Thread.Sleep( Mathf.RoundToInt( minLoadPerFrame + poisson.GetRandomValue() ) );
     }
 }
